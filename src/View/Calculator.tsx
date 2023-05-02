04beaa4817b867d4439eff189f4f0c88531fe7f8
Ruan Oliveira <ruanamaral40@gmail.com>
2023-05-01 21:14:04 -0300

    feat: creating props to define the color and size of the buttons and initial functions of the calculator

--- src/View/Calculator.tsx
+++ src/View/Calculator.tsx
@@ -1,33 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
 
 import Button from '../components/Button';
 import Display from '../components/Display';
 
 function Calculator(): JSX.Element {
-    const [displayValue, setDisplayValue] = React.useState<string>('0');
+    const [displayValue, setDisplayValue] = useState<string>('0');
+
+    const addDigit = (digit: string) => {
+        setDisplayValue(digit);
+    };
+
+    const clearMemory = () => setDisplayValue('0');
+
+    const addOperation = (operation: string) => {};
 
     return (
         <SafeAreaView style={styles.container}>
             <Display value={displayValue} />
             <View style={styles.buttons}>
-                <Button label="AC" />
-                <Button label="/" />
-                <Button label="7" />
-                <Button label="8" />
-                <Button label="9" />
-                <Button label="*" />
-                <Button label="4" />
-                <Button label="5" />
-                <Button label="6" />
-                <Button label="-" />
-                <Button label="1" />
-                <Button label="2" />
-                <Button label="3" />
-                <Button label="+" />
-                <Button label="0" />
-                <Button label="." />
-                <Button label="=" />
+                <Button label="AC" onClick={clearMemory} triple />
+                <Button label="/" onClick={addOperation} operation />
+                <Button label="7" onClick={addDigit} />
+                <Button label="8" onClick={addDigit} />
+                <Button label="9" onClick={addDigit} />
+                <Button label="*" onClick={addOperation} operation />
+                <Button label="4" onClick={addDigit} />
+                <Button label="5" onClick={addDigit} />
+                <Button label="6" onClick={addDigit} />
+                <Button label="-" onClick={addOperation} operation />
+                <Button label="1" onClick={addDigit} />
+                <Button label="2" onClick={addDigit} />
+                <Button label="3" onClick={addDigit} />
+                <Button label="+" onClick={addOperation} operation />
+                <Button label="0" onClick={addDigit} double />
+                <Button label="." onClick={addDigit} />
+                <Button label="=" onClick={addOperation} operation />
             </View>
         </SafeAreaView>
     );
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,21 +2,28 @@ import React from 'react';
 import { Text, StyleSheet, Dimensions, TouchableHighlight } from 'react-native';
 
 interface ButtonProps {
-    onClick(): void;
+    onClick(label: string): void;
     label: string;
+    operation?: boolean;
+    triple?: boolean;
+    double?: boolean;
 }
 
-const Button = ({ onClick, label }: ButtonProps) => {
+const Button = ({ onClick, label, ...rest }: ButtonProps) => {
+    const newRest = Object.keys(rest);
+
+    const stylesButton = [styles.button, newRest.map(prop => styles[prop])];
+
     return (
-        <TouchableHighlight onPress={onClick}>
-            <Text style={styles.button}>{label}</Text>
+        <TouchableHighlight onPress={() => onClick(label)}>
+            <Text style={stylesButton}>{label}</Text>
         </TouchableHighlight>
     );
 };
 
 export default Button;
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create<any>({
     button: {
         fontSize: 40,
         height: Dimensions.get('window').width / 4,
@@ -26,5 +33,15 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         borderWidth: 1,
         borderColor: '#888',
+    },
+    operation: {
+        color: 'fff',
+        backgroundColor: '#fa8231',
+    },
+    double: {
+        width: (Dimensions.get('window').width / 4) * 2,
+    },
+    triple: {
+        width: (Dimensions.get('window').width / 4) * 3,
     },
 });
